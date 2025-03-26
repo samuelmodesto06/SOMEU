00dc4f3097bb0b349811a4f98e9eaf7e351e8683
Lorenzothe <140451743+Lorenzothe@users.noreply.github.com>
2025-03-26 11:11:34 -0300

    a

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var menu = document.getElementById("menuLista");
+    menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "block" : "none";
+}
